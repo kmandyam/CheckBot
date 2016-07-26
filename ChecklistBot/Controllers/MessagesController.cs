40904f25786fcc3f7b14de0dc90591f232e0a904
Karishma Mandyam <t-karma@microsoft.com>
2016-07-26 14:13:56 -0700

    Initial commits and basic bot with NLP set up but not configured

---
+++ ChecklistBot/Controllers/MessagesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace ChecklistBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+
+                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                if (activity.Text.Equals("start checklist", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    reply = activity.CreateReply($"Sure dude, let's get a checklist started for you");
+
+                }
+
+                // return our reply to the user
+                
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+
+                return message.CreateReply("Hello BotFace!!");
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+                return message.CreateReply("Teehee I know you're typing");
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ChecklistBot/Luis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChecklistBot
+{
+
+    public class Rootobject
+    {
+        public string luis_schema_version { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string culture { get; set; }
+        public Intent[] intents { get; set; }
+        public object[] entities { get; set; }
+        public object[] composites { get; set; }
+        public object[] bing_entities { get; set; }
+        public object[] actions { get; set; }
+        public object[] model_features { get; set; }
+        public object[] regex_features { get; set; }
+        public Utterance[] utterances { get; set; }
+    }
+
+    public class Intent
+    {
+        public string name { get; set; }
+    }
+
+    public class Utterance
+    {
+        public string text { get; set; }
+        public string intent { get; set; }
+        public object[] entities { get; set; }
+    }
+
+}
